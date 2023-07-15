aeea7f60638555fdc5069d6bad7e6c1fecc517b8
fuadsaft <fuadsafat16@gmail.com>
2023-07-16 05:14:49 +0600

    adding escap sequence

---
+++ escapeSequence.cpp
@@ -0,0 +1,22 @@
+/*
+\a	Alert (bell, alarm)
+\b	Backspace
+\f	Form feed (new page)
+\n	New-line
+\r	Carriage return
+\t	Horizontal tab
+\v	Vertical tab
+\'	Single quotation mark
+\"	Double quotation mark
+\?	Question mark
+\\	Backslash
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"hello there\r";
+    return 0;
+}
