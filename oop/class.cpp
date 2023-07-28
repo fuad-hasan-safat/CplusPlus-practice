5ced661eb8a2794a451a2cd1c4b595227e34a49b
fuadsaft <fuadsafat16@gmail.com>
2023-07-29 03:52:04 +0600

    Update class.cpp
    add user manually, and check if the user already exist or not

--- oop/class.cpp
+++ oop/class.cpp
@@ -23,22 +23,34 @@ int add_user_if_not_exists(std::vector<User> &users, User user)
         {
             return i; /*returning index*/
         }
-        users.push_back(user)
-        return users.size() -1 ; /*returning index*/
-        
     }
-    
+    users.push_back(user);
+    return users.size() -1 ; /*returning index*/
 }
 
 int main(int argc, const char** argv) {
-    User me;
-    me.first_name = "Fuad Hasan";
-    me.last_name = "Hasan";
+    // User me;
+    // me.first_name = "Fuad Hasan";
+    // me.last_name = "Hasan";
+
+    // std::cout<<users[0].first_name<<std::endl;
+    
+    User user1, user2, user3;
+    user1.first_name = "Fuad";
+    user1.last_name = "Hasan";
+    user2.first_name = "Abujar";
+    user2.last_name = "Uddin";
+    user3.first_name = "Nadiya";
+    user3.last_name = "Sarkar";
     std::vector<User> users;
-    users.push_back(me);
+    users.push_back(user1);
+    users.push_back(user2);
+    users.push_back(user3);
 
-    std::cout<<users[0].first_name<<std::endl;
+    User user;
+    user.first_name = "Fuad";
+    user.last_name = "Hasan";
 
-
-    return 0;
+    std::cout<<add_user_if_not_exists(users, user)<<std::endl;
+    return 0;      
 } 
