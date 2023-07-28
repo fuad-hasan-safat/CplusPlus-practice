419e52e2ae7e127ed642f0e60a7f28875c1c477a
fuadsaft <fuadsafat16@gmail.com>
2023-07-28 20:19:12 +0600

    Create main.cpp

---
+++ oop/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+struct User
+{
+    std::string first_name;
+    std::string last_name
+    std::string status;
+};
+
+int main(int argc, const char** argv) {
+    User me;
+    me.first_name = "Fuad Hasan";
+    me.last_name = "Hasan";
+    me.status = "Gold";
+
+    std::cout << "First Name: "<< me.first_name << std::endl;
+
+    return 0;
+}
