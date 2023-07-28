ac3df7cb5ee94bc661403a2e3d2cef333f0b6dc5
fuadsaft <fuadsafat16@gmail.com>
2023-07-28 21:11:51 +0600

    add private attribute

--- oop/main.cpp
+++ oop/main.cpp
@@ -4,16 +4,23 @@ struct User
 {
     std::string first_name;
     std::string last_name;
-    std::string status;
+    std::string get_status()
+    {
+        return status;
+    }
+
+    private:
+        std::string status = "Gold";
 };
 
 int main(int argc, const char** argv) {
     User me;
     me.first_name = "Fuad Hasan";
     me.last_name = "Hasan";
-    me.status = "Gold";
+    
 
     std::cout << "First Name: "<< me.first_name << std::endl;
+    std::cout << "Status: "<< me.get_status() << std::endl;
 
     return 0;
 }
