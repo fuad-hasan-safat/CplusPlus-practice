3742e6ce77bac01dd491df6064317a56d092bcd4
fuadsaft <fuadsafat16@gmail.com>
2023-07-29 04:00:39 +0600

    Update class.cpp
    add constructor

--- oop/class.cpp
+++ oop/class.cpp
@@ -11,7 +11,16 @@ class User
         std::string get_status()
         {
             return status;
-        }   
+        }
+        User()
+        {
+            std::cout<<"Constructor\n";
+        }
+        User(std::string first_name, std::string last_name) 
+        {
+            this->first_name = first_name;
+            this->last_name = first_name;
+        }  
 };
 
 int add_user_if_not_exists(std::vector<User> &users, User user)
@@ -35,22 +44,24 @@ int main(int argc, const char** argv) {
 
     // std::cout<<users[0].first_name<<std::endl;
     
-    User user1, user2, user3;
-    user1.first_name = "Fuad";
-    user1.last_name = "Hasan";
-    user2.first_name = "Abujar";
-    user2.last_name = "Uddin";
-    user3.first_name = "Nadiya";
-    user3.last_name = "Sarkar";
-    std::vector<User> users;
-    users.push_back(user1);
-    users.push_back(user2);
-    users.push_back(user3);
+    // User user1, user2, user3;
+    // user1.first_name = "Fuad";
+    // user1.last_name = "Hasan";
+    // user2.first_name = "Abujar";
+    // user2.last_name = "Uddin";
+    // user3.first_name = "Nadiya";
+    // user3.last_name = "Sarkar";
+    // std::vector<User> users;
+    // users.push_back(user1);
+    // users.push_back(user2);
+    // users.push_back(user3);
 
-    User user;
-    user.first_name = "Fuad";
-    user.last_name = "Hasan";
+    // User user;
+    // user.first_name = "Fuad";
+    // user.last_name = "Hasan";
 
-    std::cout<<add_user_if_not_exists(users, user)<<std::endl;
+    // std::cout<<add_user_if_not_exists(users, user)<<std::endl;
+    User user("Fuad","Hasan");
+    std::cout<<user.first_name<<std::endl;
     return 0;      
 } 
