413e621b6d87e66dca53a757669f10894b756c56
fuadsaft <fuadsafat16@gmail.com>
2023-07-30 18:33:37 +0600

    Update class.cpp
    create static variable and initialize it

--- oop/class.cpp
+++ oop/class.cpp
@@ -3,30 +3,51 @@
 
 class User
 {
+    static int user_count;
     std::string status = "Gold";
 
     public:
+        static int get_user_count()
+        {
+            return user_count;
+        }
         std::string first_name;
         std::string last_name;
         std::string get_status()
         {
             return status;
         }
+        void set_status(std::string status)
+        {
+            if(status == "Gold" || status == "Silver" || status == "Bronze")
+            {
+                this->status = status;
+            }
+            else
+            {
+                this->status = "no status";
+            }
+            
+        }
         User()
         {
-            std::cout<<"Constructor\n";
+            user_count++;
         }
         User(std::string first_name, std::string last_name, std::string status) 
         {
             this->first_name = first_name;
             this->last_name = first_name;
-            this->status = status;
+            set_status(status);
+            user_count++;
         } 
         ~User()
         {
             std::cout<<"Default output\n";
+            user_count--;
         } 
 };
+
+int User::user_count = 0;
 
 int add_user_if_not_exists(std::vector<User> &users, User user)
 {
